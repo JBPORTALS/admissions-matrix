d82104dc3f10edab9c237a19b8397f77439d9471
= <=>
2023-04-26 10:31:10 +0530

    added loader screen

--- src/app/(dashboard)/dashboard/[college]/[branch]/page.tsx
+++ src/app/(dashboard)/dashboard/[college]/[branch]/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import AdmissionLayout from "@/components/layouts/AdmissionLayout";
 import { useAppDispatch } from "@/hooks";
-import {fetchSearchClass } from "@/store/admissions.slice";
-import {
-  Stack,
-  VStack,
-} from "@chakra-ui/react";
-import { useEffect} from "react";
+import { fetchSearchClass } from "@/store/admissions.slice";
+import { Stack, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
-import {useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import ClassDataGrid from "@/components/layouts/ClassDataGrid";
 
 export default function Home() {
@@ -17,16 +14,14 @@ export default function Home() {
   const router = useParams();
 
   useEffect(() => {
-    dispatch(fetchSearchClass({college:router.college,branch:router.branch}));
-  }, [router.college,router.branch]);
+    dispatch(
+      fetchSearchClass({ college: router.college, branch: router.branch })
+    );
+  }, [router.college, router.branch]);
 
   return (
-    <Stack h={"full"} w={"full"} justifyContent={"start"}>
-      <AdmissionLayout showDownloadFile>
-        <VStack h={"90vh"}>
-        <ClassDataGrid/>
-        </VStack>
-      </AdmissionLayout>
-    </Stack>
+    <VStack h={"90vh"} className="ag">
+      <ClassDataGrid />
+    </VStack>
   );
 }
