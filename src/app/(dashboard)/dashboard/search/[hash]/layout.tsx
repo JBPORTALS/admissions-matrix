087742714a5b000e89b45a82fd11feefaea223e0
= <=>
2023-04-26 15:30:25 +0530

    created styles.css

--- src/app/(dashboard)/dashboard/search/[hash]/layout.tsx
+++ src/app/(dashboard)/dashboard/search/[hash]/layout.tsx
@@ -1,34 +1,41 @@
-"use client"
+"use client";
 import { HStack, Heading, Tag, VStack } from "@chakra-ui/react";
 import { useSearchParams } from "next/navigation";
-
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-   
   const params = useSearchParams();
-     
-  return <VStack h={"full"} w={"full"}>
-    <HStack px={"5"} py={"2"} w={"full"} className="border-b border-b-lightgray">
-      
-        <Heading size={"lg"} color={"gray.600"}>Search results for</Heading>
-        {
-          params.get("type") && (
-            <HStack>
-              {
-                params.get("type")=="DATE" && (<Tag size={"lg"} colorScheme="purple">date: {params.get("date")}</Tag>)
-              }
-              {
-                params.get("type")=="SOURCE" && (<Tag size={"lg"} colorScheme="purple">Source: {params.get("source")}</Tag>)
-              }
-              </HStack>
-          )
-        }
-    </HStack>
-    {children}
-    </VStack>;
-}
 
+  return (
+    <VStack h={"full"} w={"full"}>
+      <HStack
+        px={"5"}
+        py={"2"}
+        w={"full"}
+        className="border-b bg border-b-lightgray"
+      >
+        <Heading size={"lg"} color={"gray.600"}>
+          Search results for
+        </Heading>
+        {params.get("type") && (
+          <HStack>
+            {params.get("type") == "DATE" && (
+              <Tag size={"lg"} colorScheme="purple">
+                date: {params.get("date")}
+              </Tag>
+            )}
+            {params.get("type") == "SOURCE" && (
+              <Tag size={"lg"} colorScheme="purple">
+                Source: {params.get("source")}
+              </Tag>
+            )}
+          </HStack>
+        )}
+      </HStack>
+      {children}
+    </VStack>
+  );
+}
--- src/app/(dashboard)/dashboard/search/[hash]/page.tsx
+++ src/app/(dashboard)/dashboard/search/[hash]/page.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { useParams, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import "../../../../globals.css"
+import "./styles.css"
 
 export default function Page() {
   const [data, setData] = useState();
