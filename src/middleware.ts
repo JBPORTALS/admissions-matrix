a423756e3d5c20cb972c859ca0922461a278c087
Manu <manu48617@gmail.com>
2025-06-10 11:20:40 +0530

    Implement public path handling in middleware to allow unauthenticated access to specific routes. This change enhances user experience by enabling access to the home and new enquiry pages without authentication.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,14 @@ import { NextRequest } from "next/server";
 import { getIronSession } from "iron-session";
 import { sessionOptions, SessionData } from "@/utils/session";
 
+const publicPaths = ["/", "/new-enquiry"];
+
 export default async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const session = await getIronSession<SessionData>(req, res, sessionOptions);
 
-  if (req.nextUrl.pathname === "/") return NextResponse.next();
+  //If it's a public paths let user vists the page
+  if (publicPaths.includes(req.nextUrl.pathname)) return NextResponse.next();
 
   // Protect routes that require authentication
   if (!session.id && !req.nextUrl.pathname.startsWith("/signin")) {
